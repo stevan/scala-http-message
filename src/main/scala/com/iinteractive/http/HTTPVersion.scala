5df3b74a3afdd414e838f25edf55d4a3c6233036
Stevan Little <stevan.little@iinteractive.com>
2013-03-14 09:35:16 -0400

    HTTP versions

--- src/main/scala/com/iinteractive/http/HTTPVersion.scala
+++ src/main/scala/com/iinteractive/http/HTTPVersion.scala
@@ -1,12 +1,36 @@
 package com.iinteractive.http
 
-class HTTPVersion {
+object HTTPVersion extends Enumeration {
+    type HTTPVersion = HTTPVersionValue
 
+    case class HTTPVersionValue(major: Int, minor: Int) extends Val(nextId) {
+        def protocolName = "HTTP"
+        def majorVersion = major
+        def minorVersion = minor
+        override def toString = protocolName + "/" + majorVersion + "." + minorVersion
+    }
+
+    val HTTP_0_9 = HTTPVersionValue(0, 9)
+    val HTTP_1_0 = HTTPVersionValue(1, 0)
+    val HTTP_1_1 = HTTPVersionValue(1, 1)
+
+    // NOTE:
+    // not sure I actually need this or care about it
+    // it will require adding the following line at
+    // the top of this file:
+    //    import scala.language.implicitConversions
+    // but lets wait until we actually need or care.
+    // - SL
+    //implicit def valueToVersion(v: Value): HTTPVersionValue = v.asInstanceOf[HTTPVersionValue]
 }
 
 /**
  * See Also:
  *
+ * Enumeration.Val extension:
+ * - http://downgra.de/2010/02/11/playing-with-scala-enumeration/
+ *
+ * Other implementations:
  * - http://hc.apache.org/httpcomponents-core-ga/httpcore/xref/org/apache/http/HttpVersion.html
  * - https://github.com/netty/netty/blob/master/codec-http/src/main/java/io/netty/handler/codec/http/HttpVersion.java
  */
---
+++ src/test/scala/com/iinteractive/http/HTTPVersionTestSuite001.scala
@@ -0,0 +1,19 @@
+package com.iinteractive.http
+
+import com.iinteractive.test._
+
+class HTTPVersionTestSuite001 extends TestMore {
+
+    is(HTTPVersion.HTTP_0_9.toString, "HTTP/0.9", "... got the expected value")
+    is(HTTPVersion.HTTP_0_9.majorVersion, 0, "... got the expected value")
+    is(HTTPVersion.HTTP_0_9.minorVersion, 9, "... got the expected value")
+
+    is(HTTPVersion.HTTP_1_0.toString, "HTTP/1.0", "... got the expected value")
+    is(HTTPVersion.HTTP_1_0.majorVersion, 1, "... got the expected value")
+    is(HTTPVersion.HTTP_1_0.minorVersion, 0, "... got the expected value")
+
+    is(HTTPVersion.HTTP_1_1.toString, "HTTP/1.1", "... got the expected value")
+    is(HTTPVersion.HTTP_1_1.majorVersion, 1, "... got the expected value")
+    is(HTTPVersion.HTTP_1_1.minorVersion, 1, "... got the expected value")
+
+}
