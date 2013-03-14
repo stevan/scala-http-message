35cfbe7816b823cc36899cc67ce50bab43ab0036
Stevan Little <stevan.little@iinteractive.com>
2013-03-14 11:49:23 -0400

    convert HTTP Method to non-Enum

--- src/main/scala/com/iinteractive/http/HTTPMethod.scala
+++ src/main/scala/com/iinteractive/http/HTTPMethod.scala
@@ -1,24 +1,40 @@
 package com.iinteractive.http
 
-object HTTPMethod extends Enumeration {
-    type HTTPMethod = Value
 
-    val OPTIONS = Value("OPTIONS")
-    val GET     = Value("GET")
-    val HEAD    = Value("HEAD")
-    val POST    = Value("POST")
-    val PUT     = Value("PUT")
-    val DELETE  = Value("DELETE")
-    val PATCH   = Value("PATCH")
-    val TRACE   = Value("TRACE")
-    val CONNECT = Value("CONNECT")
+sealed abstract class HTTPMethod(private val name: String) {
+    def getName = name
 
-    def apply(x: String) = withName(x)
+    override def toString = getName
+}
+
+object HTTPMethod {
+    case object OPTIONS extends HTTPMethod("OPTIONS")
+    case object GET     extends HTTPMethod("GET")
+    case object HEAD    extends HTTPMethod("HEAD")
+    case object POST    extends HTTPMethod("POST")
+    case object PUT     extends HTTPMethod("PUT")
+    case object DELETE  extends HTTPMethod("DELETE")
+    case object PATCH   extends HTTPMethod("PATCH")
+    case object TRACE   extends HTTPMethod("TRACE")
+    case object CONNECT extends HTTPMethod("CONNECT")
+
+    def withName(n: String): HTTPMethod = n.toUpperCase match {
+        case "OPTIONS" => OPTIONS
+        case "GET"     => GET
+        case "HEAD"    => HEAD
+        case "POST"    => POST
+        case "PUT"     => PUT
+        case "DELETE"  => DELETE
+        case "PATCH"   => PATCH
+        case "TRACE"   => TRACE
+        case "CONNECT" => CONNECT
+    }
 }
 
 /**
  * See Also:
  *
+ * Other Implementations
  * - https://github.com/netty/netty/blob/master/codec-http/src/main/java/io/netty/handler/codec/http/HttpMethod.java
  * - http://java.net/projects/jax-rs-spec/sources/git/content/src/jax-rs-api/src/main/java/javax/ws/rs/HttpMethod.java
  */
--- src/test/scala/com/iinteractive/http/HTTPMethodTestSuite001.scala
+++ src/test/scala/com/iinteractive/http/HTTPMethodTestSuite001.scala
@@ -4,7 +4,7 @@ import com.iinteractive.test._
 
 class HTTPMethodTestSuite001 extends TestMore {
 
-    is(HTTPMethod("GET"), HTTPMethod.GET, "... got the expected value")
+    is(HTTPMethod.GET.toString, "GET", "... got the expected value")
     is(HTTPMethod.withName("GET"), HTTPMethod.GET, "... got the expected value")
 
 }
