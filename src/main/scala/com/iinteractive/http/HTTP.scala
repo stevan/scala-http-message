ab862180cc76d7a0cc6045700be1b1fea8e5c107
Stevan Little <stevan.little@iinteractive.com>
2013-03-15 23:13:42 -0400

    date exception

--- src/main/scala/com/iinteractive/http/HTTP.scala
+++ src/main/scala/com/iinteractive/http/HTTP.scala
@@ -5,6 +5,7 @@ object HTTP {
     object Errors {
         class InvalidHTTPMethod  (msg: String) extends Exception(msg)
         class InvalidHTTPVersion (msg: String) extends Exception(msg)
+        class InvalidHTTPDate    (msg: String) extends Exception(msg)
     }
 
 }
