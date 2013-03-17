750346e5221aef53d3733579582b75034717c1f7
Stevan Little <stevan.little@iinteractive.com>
2013-03-16 21:28:35 -0400

    remove these bad tests

--- src/test/scala/com/iinteractive/http/HTTPHeadersTestSuite001.scala
+++ src/test/scala/com/iinteractive/http/HTTPHeadersTestSuite001.scala
@@ -3,15 +3,9 @@ package com.iinteractive.http
 import com.iinteractive.test._
 import com.iinteractive.http.headers._
 
-import java.util.Date
+import org.joda.time.DateTime
 
 class HTTPHeadersTestSuite001 extends TestMore {
-
-    val a = new HTTPHeader.Accept("Foo")
-    diag(a.value)
-
-    val d = new HTTPHeader.Date(new HTTPDate(new Date()))
-    diag(d.value)
 
     pass("... placeholder")
 
