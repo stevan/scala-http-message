6ae7efd0a6381df9f2d914585c54fc61f581c847
Stevan Little <stevan.little@iinteractive.com>
2013-03-16 21:28:08 -0400

    priority list test

--- src/test/scala/com/iinteractive/http/headers/PriorityListTestSuite001.scala
+++ src/test/scala/com/iinteractive/http/headers/PriorityListTestSuite001.scala
@@ -6,15 +6,16 @@ class PriorityListTestSuite001 extends TestMore {
 
     val pl = new PriorityList()
 
+    pl.add(new MediaType("audio", "mp3"))
     pl.add(new MediaType("text", "html", 0.5))
     pl.add(new MediaType("text", "plain"))
     pl.add(new MediaType("text", "xml", 0.2))
 
     val i = pl.iterator
-    while (i.hasNext) {
-        diag(i.next().toString)
-    }
-
-    pass("... placeholder")
+    
+    is(i.next().toString, "audio/mp3", "... got the top priority")
+    is(i.next().toString, "text/plain", "... got the next priority")
+    is(i.next().toString, "text/html", "... got the next priority")
+    is(i.next().toString, "text/xml", "... got the next priority")
 
 }
