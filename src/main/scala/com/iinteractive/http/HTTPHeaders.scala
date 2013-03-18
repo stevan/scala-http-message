ed10b40fafc6a4a42f06a4859ffa4e13ccd2d7f0
Stevan Little <stevan.little@iinteractive.com>
2013-03-18 11:13:59 -0400

    converting more of the headers to the right types

--- src/main/scala/com/iinteractive/http/HTTPHeaders.scala
+++ src/main/scala/com/iinteractive/http/HTTPHeaders.scala
@@ -12,19 +12,10 @@ sealed abstract class HTTPHeader(val name: String) {
 object HTTPHeader {
     def unapply(h: HTTPHeader): Option[(String, String)] = Some(h.name -> h.value)
 
-    case class Accept         (val value: String) extends HTTPHeader("Accept")
-
-    case class AcceptCharset  (val list: PriorityList) extends HTTPHeader("Accept-Charset") {
-        def value = list.toString
-    }
-
-    case class AcceptEncoding (val list: PriorityList) extends HTTPHeader("Accept-Encoding") {
-        def value = list.toString
-    }
-    
-    case class AcceptLanguage (val list: PriorityList) extends HTTPHeader("Accept-Language") {
-        def value = list.toString
-    }
+    case class Accept         (val list: MediaTypeList) extends HTTPHeader("Accept")          { def value = list.toString }
+    case class AcceptCharset  (val list: PriorityList)  extends HTTPHeader("Accept-Charset")  { def value = list.toString }
+    case class AcceptEncoding (val list: PriorityList)  extends HTTPHeader("Accept-Encoding") { def value = list.toString }
+    case class AcceptLanguage (val list: PriorityList)  extends HTTPHeader("Accept-Language") { def value = list.toString }
 
     case class AcceptRanges   (val value: String) extends HTTPHeader("Accept-Ranges")
     case class AcceptPatch    (val value: String) extends HTTPHeader("Accept-Patch")
